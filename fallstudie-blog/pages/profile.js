235d78412c956ec473a9eead01fd386957d36d6f
Reiter Gregory <breitg@bbcag.ch>
2023-04-20 12:43:41 +0200

    added images

--- fallstudie-blog/pages/profile.js
+++ fallstudie-blog/pages/profile.js
@@ -5,14 +5,31 @@ export default function ProfilePage({ session }) {
     useRedirectToLogin(session)
 
     return (
-        session && session.user && (
-            <div className={styles.container}>
+        session.user && (
+            <div className={styles.container} style={{ display: 'flex', justifyContent: 'center', alignContent:"center", flexDirection:"column"}}>
                 <div className={styles.profileBox}>
                     <h2 className={styles.profileName}>{session.user.name}</h2>
 
                 </div>
+                <div style={{ display: 'flex', justifyContent: 'center', alignContent:"center", flexDirection:"row", gap:"2rem"}}>
+                    <img
+                        src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/Vladimir_Lenin.jpg/1200px-Vladimir_Lenin.jpg"
+                        style={{ display: 'flex', justifyContent: 'center', width: '23%', }}
+                    />
+                    <img
+                        src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Karl_Marx_001.jpg/1200px-Karl_Marx_001.jpg"
+                        style={{ display: 'flex', justifyContent: 'center', width: '23%'}}
+                    />
+                    <img
+                        src="https://3.bp.blogspot.com/-zOD1JKjD3JI/V76JNs9OEwI/AAAAAAAAK3U/Pxq1BkjxcwAhOPwHJrqQO1kMaDepKdXWACLcB/s1600/socialist_kiss_brezhnev_honecker_1979.jpg"
+                        style={{ display: 'flex', justifyContent: 'center', width: '40%'}}
+                    />
+                </div>
+
             </div>
         )
     )
+
+
 
 }
